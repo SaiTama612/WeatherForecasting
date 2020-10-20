b83435abe589f493b2b1c4bb20ba6b42037de779
SHUBHAM RAJ <rajs17224@gmail.com>
2020-10-20 22:26:53 +0530

    Initial Commit

---
+++ src/components/citylist.vue
@@ -0,0 +1,29 @@
+<template>
+ <div>
+  <h1>citylist</h1>
+ </div>
+    
+</template>
+<script>
+import Vue from 'vue';
+import VueAxios from 'vue-axios';
+import axios from 'axios'
+Vue.use(VueAxios,axios)
+export default {
+    name:"citylist",
+    data(){
+        return{
+            list:undefined
+        }
+    },
+    mounted(){
+        Vue.axios.get('https://indian-cities-api-nocbegfhqg.now.sh/cities')
+        .then(resp =>{
+            this.list=resp.data.data
+            console.warn(resp.data.data)
+
+        })
+    }
+    
+}
+</script>
